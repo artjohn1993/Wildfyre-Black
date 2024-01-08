21cd1ccfac84f0135221c6884baee9b594b33e9b
artjohn29 <30612488+artjohn1993@users.noreply.github.com>
2024-01-08 18:02:18 +0800

    update to WILDFYRE

--- app/src/main/java/com/generator/wildfyreblacknxtgen/activity/MainActivity.kt
+++ app/src/main/java/com/generator/wildfyreblacknxtgen/activity/MainActivity.kt
@@ -60,9 +60,9 @@ class MainActivity : AppCompatActivity(), GoogleSheetView {
         try {
             supportActionBar?.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM)
             supportActionBar?.setCustomView(R.layout.custom_action_bar)
-            action_bar_title.setText("WildFyre NXTGEN")
-            action_bar_subtitle.setText("v.20240104.1")
-                //Wildfyre_NXTGEN_20240104.1
+            action_bar_title.setText("WILDFYRE NXTGEN")
+            action_bar_subtitle.setText("v.20240108.1")
+                //Wildfyre_NXTGEN_20240108.1
         } catch (e: PackageManager.NameNotFoundException) {
             e.printStackTrace()
         }
