0c39f79095fe8e61122a3f7ce6d8c42d9cd4e6f5
artjohn29 <30612488+artjohn1993@users.noreply.github.com>
2024-01-08 18:05:59 +0800

    change to wildfyre

--- app/src/main/java/com/generator/special/activity/MainActivity.kt
+++ app/src/main/java/com/generator/special/activity/MainActivity.kt
@@ -60,9 +60,9 @@ class MainActivity : AppCompatActivity(), GoogleSheetView {
         try {
             supportActionBar?.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM)
             supportActionBar?.setCustomView(R.layout.custom_action_bar)
-            action_bar_title.setText("WildFyre SPECIAL")
-            action_bar_subtitle.setText("v.20240104.1")
-            //Wildfyre_SPECIAL_20240104.1
+            action_bar_title.setText("WILDFYRE SPECIAL")
+            action_bar_subtitle.setText("v.20240108.1")
+            //Wildfyre_SPECIAL_20240108.1
         } catch (e: PackageManager.NameNotFoundException) {
             e.printStackTrace()
         }
